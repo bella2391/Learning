91fe6807743ed2d98d8f6d8ddd12f2179cc0bb83
bella <maekawatakaya10.8@gmail.com>
2024-12-20 22:39:36 +0900

    Learning from https://www.tohoho-web.com/ex/typescript.html

---
+++ sample/sample.ts
@@ -0,0 +1,23 @@
+function hello(name: string): void {
+    console.log('Hello ' + name + '!');
+}
+let your_name: string = 'Yamada';
+hello(your_name);
+
+let v_bool: boolean = true;
+let v_num: number = 123;
+let v_str: string = 'ABCDEFG';
+let v_null: null = null;
+let v_undef: undefined = undefined;
+
+let v_arr1: string[] = ['Red', 'Green', 'Blue'];
+let v_arr2: Array<String> = ['Red', 'Green', 'Blue'];
+let v_tuple: [string, number] = ['Yamada', 26];
+
+let v_obj: object = {'name': 'yamada', 'age': 26};
+
+enum Color {Red, Green, Blue};
+let v_enm: Color = Color.Green;
+
+class MyClass { name: String; }
+var val_class: MyClass = new MyClass();
