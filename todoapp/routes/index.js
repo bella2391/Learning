5406f4cc0a691887900485445432e42706671616
bella <maekawatakaya10.8@gmail.com>
2024-12-25 05:00:20 +0900

    Using knex module first. Connect MySQL.

--- todoapp/routes/index.js
+++ todoapp/routes/index.js
@@ -3,25 +3,50 @@ const router = express.Router();
 const mysql = require('mysql');
 
 const conn = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: ''
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE
 });
 
-let todos = [];
+//let todos = [];
 
 router.get('/', function(req, res, next) {
-    res.render('index', {
-        title: 'ToDo App',
-        todos: todos,
-    });
+    conn.query(
+        `SELECT * FROM tasks;`,
+        (error, results) => {
+            if (error) {
+                console.log(error);
+                return
+            }
+            
+            console.log(results);
+            res.render('index', {
+                title: 'ToDo App',
+                todos: results,
+            })
+        }
+    )
 });
 
 router.post('/', function(req, res, next) {
+    conn.connect(err => {
+        if (err) {
+            console.log('error connecting: ' + err.stack);
+            return
+        }
+        console.log('mysql connection established!');
+    })
     const todo = req.body.add;
-    todos.push(todo);
-    res.redirect('/');
+    conn.query(
+        `INSERT INTO tasks (user_id, content) VALUES (1, '${todo}');`,
+        (error, results) => {
+            console.log(error);
+            res.redirect('/');
+        }
+    )
+    //todos.push(todo);
+    //res.redirect('/');
 });
 
 module.exports = router;
