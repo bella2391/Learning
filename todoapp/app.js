9414554aa05fb55cd46f3a81b036714f32222f53
bella <maekawatakaya10.8@gmail.com>
2024-12-25 09:35:47 +0900

    Introducing function of signup/in. ErrorMessage by ejs file is managed under one file. This is because using `<%- include('./'); %>`.

--- todoapp/app.js
+++ todoapp/app.js
@@ -3,11 +3,23 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const cookieSession = require('cookie-session');
+const secret = 'secretCuisine123';
 
 //const indexRouter = require('./routes/index');
 //const usersRouter = require('./routes/users');
 
 const app = express();
+
+app.use(
+    cookieSession({
+        name: "session",
+        keys: [secret],
+
+        // Cookie Options
+        maxAge: 24 * 60 * 60 * 1000, // 24 hours
+    })
+);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -24,18 +36,18 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use("/", require("./routes"));
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+    next(createError(404));
 });
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 module.exports = app;
