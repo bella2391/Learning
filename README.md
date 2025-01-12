f5a4218a0bee0520a179ea4f80d12cead000dc47
bella <maekawatakaya10.8@gmail.com>
2025-01-12 22:20:04 +0900

    Setup discord.py environment.

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Python
+
+## Env
+
+```env
+TOKEN=thisisdiscordtoken
+```
---
+++ main.py
@@ -0,0 +1,15 @@
+import discord
+
+from config.config import TOKEN
+
+intents = discord.Intents.default()
+
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print('login successfully')
+    game = discord.Game('Python')
+    await client.change_presence(status=discord.Status.online, activity=game)
+
+client.run(TOKEN)
