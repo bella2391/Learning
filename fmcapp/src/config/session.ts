c2a0e51cfe7af87ef4ba98e896c2667aa8efe086
bella <maekawatakaya10.8@gmail.com>
2025-01-14 10:04:54 +0900

    not using exported app instance for session.ts

--- fmcapp/src/config/session.ts
+++ fmcapp/src/config/session.ts
@@ -1,16 +1,9 @@
-import * as dotenv from 'dotenv';
-import * as path from 'path';
-const envPath = path.resolve(__dirname, '../.env');
-dotenv.config({ path: envPath });
-
-import { Application } from 'express';
 import session from 'express-session';
-
-const sessionSecret = process.env.COOKIE_SECRET || 'defaultSecret';
-
 import connectSessionSequelize from 'connect-session-sequelize';
 import { Sequelize } from 'sequelize';
+import './';
 
+const sessionSecret = process.env.COOKIE_SECRET || 'defaultSecret';
 const SequelizeStore = connectSessionSequelize(session.Store);
 
 const sequelize = new Sequelize(process.env.MYSQL_DATABASE || '', process.env.MYSQL_USER || '', process.env.MYSQL_PASSWORD || undefined, {
@@ -19,21 +12,15 @@ const sequelize = new Sequelize(process.env.MYSQL_DATABASE || '', process.env.MY
     logging: false,
 });
 
-export default (app: Application) => {
-    app.use(
-        session({
-            secret: sessionSecret,
-            store: new SequelizeStore({
-                db: sequelize,
-            }),
-            resave: false,
-            saveUninitialized: false,
-            cookie: {
-                maxAge: 24 * 60 * 60 * 1000,
-                //secure: process.env.NODE_ENV === 'production',
-            },
-        })
-    );
-
-    sequelize.sync();
-};
+export default session({
+    secret: sessionSecret,
+    store: new SequelizeStore({
+        db: sequelize,
+    }),
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        maxAge: 24 * 60 * 60 * 1000,
+        //secure: process.env.NODE_ENV === 'production',
+    },
+});
