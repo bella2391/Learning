89bfc699140096b0f6a374a8afbd14e6dd8a22f2
bella <maekawatakaya10.8@gmail.com>
2025-01-16 12:55:47 +0900

    using controller

--- fmcapp/src/routes/signin.ts
+++ fmcapp/src/routes/signin.ts
@@ -1,6 +1,6 @@
 import express, { Request, Response, NextFunction } from 'express';
-import passport from 'passport';
-import basepath from '../util/basepath';
+import { requireNonLogin } from '../middlewares/checker';
+import { commonAuth } from '../controllers/authController';
 
 const router: express.Router = express.Router();
 
@@ -14,28 +14,6 @@ router.get('/', (req: Request, res: Response, _: NextFunction) => {
     res.render('signin', { title: 'Sign in' });
 });
 
-router.post('/', (req: Request, res: Response, next: NextFunction) => {
-    passport.authenticate('local', (err, user, info) => {
-        if (err) {
-            return next(err);
-        }
-        if (!user) {
-            if (info && info.redirectUrl) {
-                return res.redirect(info.redirectUrl);
-            }
-            return res.status(401).send(info.message || 'Authentication failed')
-        }
-        req.login(user, (err) => {
-            if (err) {
-                return next(err);
-            }
-            if (req.session.n) {
-                return res.redirect('/minecraft/uuid_check.php');
-            } else {
-                return res.redirect(`${basepath.rootpath}/`);
-            }
-        });
-    })(req, res, next);
-});
+router.post('/', requireNonLogin, commonAuth('local'));
 
 export default router;
