a52b90febb829abeb11365f887fd4adbc735b318
bella <maekawatakaya10.8@gmail.com>
2025-01-16 10:20:38 +0900

    change so that not using like `export default (app: Application) => {` when reading module in app.ts, and don't concentrate too much code there

--- todoapp/src/app.ts
+++ todoapp/src/app.ts
@@ -2,22 +2,24 @@ import createError from 'http-errors';
 import express, { Request, Response, NextFunction } from 'express';
 import path from 'path';
 import cookieParser from 'cookie-parser';
-import { indexRouter } from './routes/index';
 import logger from 'morgan';
-import session from './config/session';
-import passport from './config/passport';
 import flash from 'connect-flash';
-import basepath from './util/basepath';
-import cors from 'cors';
-import csurf from './sec/csurf';
-import localvals from './config/localvals';
+import favicon from 'serve-favicon';
+import { indexRouter } from './routes/index';
+import session from './middlewares/session';
+import passport from './middlewares/passport';
+import basepath from './utils/basepath';
+import localvals from './middlewares/localvals';
+import preErrorHandler from './middlewares/pre-error-handler';
+import csrfProtection from './middlewares/csurf';
+import mycors from './middlewares/cors';
+import errorHandler from './middlewares/error-handler';
 
 const app = express();
 
 console.log(`-- current mode is ${process.env.NODE_ENV} --`);
 console.log('-- current Base URL is ' + basepath.rooturl);
 
-// view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
@@ -28,6 +30,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 
 app.use(session);
 
@@ -36,41 +39,18 @@ app.use(flash());
 app.use(passport.initialize());
 app.use(passport.session());
 
-csurf(app);
+app.use(csrfProtection);
 
-// global ejs template variables
 app.use(localvals);
 
-// router
+app.use(preErrorHandler);
+
 app.use('/', indexRouter);
 
-// debug
-app.use(cors({
-    origin: (origin, callback) => {
-        const allowedOrigins = ['http://localhost:3001', 'http://example.com']; // 許可するドメイン
-        if (!origin || allowedOrigins.includes(origin)) {
-            callback(null, true);
-        } else {
-            callback(new Error('Not allowed by CORS'));
-        }
-    },
-    credentials: true, // クッキーの送信を許可
-}));
+app.use(mycors);
 
-// catch 404 and forward to error handler
-app.use((_: Request, __: Response, next: NextFunction) => {
-    next(createError(404));
-});
+app.use((_: Request, __: Response, next: NextFunction) => next(createError(404)));
 
-// error handler
-app.use((err: createError.HttpError, req: Request, res: Response, _: NextFunction) => {
-    // set locals, only providing error in development
-    res.locals.message = err.message;
-    res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-    // render the error page
-    res.status(err.status || 500);
-    res.render('./include/error');
-});
+app.use(errorHandler);
 
 export default app;
