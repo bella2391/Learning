39d0c0f462a9f757a36f161302522ddf23a31b90
bella <maekawatakaya10.8@gmail.com>
2025-01-16 10:49:26 +0900

    once the sign-up>email confirmation>link click is complete, exclude the second sign-in and redirect on successful authentication

--- fmcapp/src/routes/auth.ts
+++ fmcapp/src/routes/auth.ts
@@ -2,14 +2,38 @@ import express, { Request, Response } from 'express';
 import passport from 'passport';
 import { JwtPayload } from 'jsonwebtoken';
 import '../config';
-import basepath from '../util/basepath';
+import basepath from '../utils/basepath';
 import knex from '../db/knex';
-import authenticateJWT, { generateToken, getToken } from '../config/jwt';
-import { sendVertificationEmail } from '../mail/mail';
+import authenticateJWT, { generateToken, getToken } from '../middlewares/jwt';
+import { sendVertificationEmail } from '../controllers/emailController';
+import { requireNonLogin } from '../middlewares/checker';
 
 const router: express.Router = express.Router();
 
-router.get('/set-email', authenticateJWT, async (req: Request, res: Response) => {
+async function loginRedirect(req: Request, res: Response, data: any, timeout: number = 3000) {
+    if (!req.payload) {
+        res.status(400).send('Invalid Payload');
+        throw new Error("Invalid Payload");
+    }
+
+    if (typeof data.redirect_url != 'string') {
+        throw new Error("Must be included redirect_url: string in data");
+    }
+
+    data['timeout'] = timeout;
+
+    const user = await knex('users').where({ id: req.payload.id, name: req.payload.name }).first();
+    req.login(user, (err) => {
+        if (err) {
+            res.status(500).send('Error logging in');
+            return;
+        }
+
+        res.render('redirect', data);
+    });
+}
+
+router.get('/set-email', requireNonLogin, authenticateJWT, async (req: Request, res: Response) => {
     if (!req.payload) {
         return;
     }
@@ -19,7 +43,11 @@ router.get('/set-email', authenticateJWT, async (req: Request, res: Response) =>
         if (check) {
             try {
                 await knex('users').where({ id: req.payload.id }).update({ email: req.payload2.email });
-                res.render('signin', { title: 'Signin', successMessage: [ 'Email setting done successfully' ] });
+
+                await loginRedirect(req, res, {
+                    'redirect_url': basepath.successurl,
+                    'successMessage': [ 'Email setting done successfully' ],
+                });
             } catch (err) {
                 res.status(500).send('Error updating email.');
             }
@@ -33,7 +61,7 @@ router.get('/set-email', authenticateJWT, async (req: Request, res: Response) =>
     res.render('auth/verify-form', { token, title: 'email setting', auth_path: '/set-email', label: 'メールアドレス', input_name: 'email', });
 });
 
-router.post('/set-email', authenticateJWT, async (req: Request, res: Response) => {
+router.post('/set-email', requireNonLogin, authenticateJWT, async (req: Request, res: Response) => {
     if (!req.payload) {
         throw new Error('Invalid Access.');
     }
@@ -55,7 +83,7 @@ router.post('/set-email', authenticateJWT, async (req: Request, res: Response) =
     }
 });
 
-router.get('/verify-otp', authenticateJWT, async (req: Request, res: Response) => {
+router.get('/verify-otp', requireNonLogin, authenticateJWT, async (req: Request, res: Response) => {
     if (!req.payload) {
         return;
     }
@@ -65,7 +93,7 @@ router.get('/verify-otp', authenticateJWT, async (req: Request, res: Response) =
     res.render('auth/verify-form', { token, title: 'otp confirm', auth_path: '/verify-otp', label: 'ワンタイムパスワード', input_name: 'otp', });
 });
 
-router.post('/verify-otp', authenticateJWT, async (req: Request, res: Response): Promise<void> => {
+router.post('/verify-otp', requireNonLogin, authenticateJWT, async (req: Request, res: Response): Promise<void> => {
     if (!req.payload) {
         return;
     }
@@ -88,32 +116,32 @@ router.post('/verify-otp', authenticateJWT, async (req: Request, res: Response):
                 return;
             }
 
-            res.redirect(`${basepath.rootpath}/`);
+            res.redirect(basepath.successurl);
         });
     } catch (error) {
         res.status(500).send('Error verifying OTP.');
     }
 });
 
-router.get('/discord', passport.authenticate('discord'));
+router.get('/discord', requireNonLogin, passport.authenticate('discord'));
 
-router.get('/discord/callback', passport.authenticate('discord', {
+router.get('/discord/callback', requireNonLogin, passport.authenticate('discord', {
     failureRedirect: `${basepath.rootpath}/signin`,
-    successRedirect: `${basepath.rootpath}/`,
+    successRedirect: basepath.successurl,
 }));
 
-router.get('/google', passport.authenticate('google', { scope: ['profile', 'email'] }));
+router.get('/google', requireNonLogin, passport.authenticate('google', { scope: ['profile', 'email'] }));
 
-router.get('/google/callback', passport.authenticate('google', {
+router.get('/google/callback', requireNonLogin, passport.authenticate('google', {
     failureRedirect: `${basepath.rootpath}/signin`,
-    successRedirect: `${basepath.rootpath}/`
+    successRedirect: basepath.successurl,
 }));
 
-router.get('/x', passport.authenticate('x'));
+router.get('/x', requireNonLogin, passport.authenticate('x'));
 
-router.get('/x/callback', passport.authenticate('x', {
+router.get('/x/callback', requireNonLogin, passport.authenticate('x', {
     failureRedirect: `${basepath.rootpath}/signin`,
-    successRedirect: `${basepath.rootpath}/`
+    successRedirect: basepath.successurl,
 }));
 
 export default router;
