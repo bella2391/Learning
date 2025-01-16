cf5e35787d02b87c9eea1e9f51db72e7fc35c376
bella <maekawatakaya10.8@gmail.com>
2025-01-16 12:45:50 +0900

    setup commonAuth for all login method, mainly redirecting

--- fmcapp/src/routes/auth.ts
+++ fmcapp/src/routes/auth.ts
@@ -1,5 +1,4 @@
-import express, { Request, Response } from 'express';
-import passport from 'passport';
+import express, { Request, Response, NextFunction } from 'express';
 import { JwtPayload } from 'jsonwebtoken';
 import '../config';
 import basepath from '../utils/basepath';
@@ -7,34 +6,15 @@ import knex from '../db/knex';
 import authenticateJWT, { generateToken, getToken } from '../middlewares/jwt';
 import { sendVertificationEmail } from '../controllers/emailController';
 import { requireNonLogin } from '../middlewares/checker';
+import { loginRedirect, setupAuthRoutes } from '../controllers/authController';
 
 const router: express.Router = express.Router();
 
-async function loginRedirect(req: Request, res: Response, data: any, timeout: number = 3000) {
+setupAuthRoutes(router, [ 'google', 'x', 'discord' ]);
+
+router.get('/set-email', requireNonLogin, authenticateJWT, async (req: Request, res: Response, next: NextFunction) => {
     if (!req.payload) {
         res.status(400).send('Invalid Payload');
-        throw new Error("Invalid Payload");
-    }
-
-    if (typeof data.redirect_url != 'string') {
-        throw new Error("Must be included redirect_url: string in data");
-    }
-
-    data['timeout'] = timeout;
-
-    const user = await knex('users').where({ id: req.payload.id, name: req.payload.name }).first();
-    req.login(user, (err) => {
-        if (err) {
-            res.status(500).send('Error logging in');
-            return;
-        }
-
-        res.render('redirect', data);
-    });
-}
-
-router.get('/set-email', requireNonLogin, authenticateJWT, async (req: Request, res: Response) => {
-    if (!req.payload) {
         return;
     }
 
@@ -44,10 +24,9 @@ router.get('/set-email', requireNonLogin, authenticateJWT, async (req: Request, 
             try {
                 await knex('users').where({ id: req.payload.id }).update({ email: req.payload2.email });
 
-                await loginRedirect(req, res, {
-                    'redirect_url': basepath.successurl,
-                    'successMessage': [ 'Email setting done successfully' ],
-                });
+                const user = await knex('users').where({ id: req.payload.id, name: req.payload.name }).first();
+
+                loginRedirect(req, res, next, user, { 'successMessage': [ 'Email setting done successfully' ] });
             } catch (err) {
                 res.status(500).send('Error updating email.');
             }
@@ -63,6 +42,7 @@ router.get('/set-email', requireNonLogin, authenticateJWT, async (req: Request, 
 
 router.post('/set-email', requireNonLogin, authenticateJWT, async (req: Request, res: Response) => {
     if (!req.payload) {
+        res.status(400).send('Invalid Payload');
         throw new Error('Invalid Access.');
     }
 
@@ -85,6 +65,7 @@ router.post('/set-email', requireNonLogin, authenticateJWT, async (req: Request,
 
 router.get('/verify-otp', requireNonLogin, authenticateJWT, async (req: Request, res: Response) => {
     if (!req.payload) {
+        res.status(400).send('Invalid Payload');
         return;
     }
 
@@ -93,8 +74,9 @@ router.get('/verify-otp', requireNonLogin, authenticateJWT, async (req: Request,
     res.render('auth/verify-form', { token, title: 'otp confirm', auth_path: '/verify-otp', label: 'ワンタイムパスワード', input_name: 'otp', });
 });
 
-router.post('/verify-otp', requireNonLogin, authenticateJWT, async (req: Request, res: Response): Promise<void> => {
+router.post('/verify-otp', requireNonLogin, authenticateJWT, async (req: Request, res: Response, next: NextFunction): Promise<void> => {
     if (!req.payload) {
+        res.status(400).send('Invalid Payload');
         return;
     }
 
@@ -110,38 +92,11 @@ router.post('/verify-otp', requireNonLogin, authenticateJWT, async (req: Request
         await knex('users').where({ id: req.payload.id, name: req.payload.name, otp }).update({ otp: null });
 
         const user = await knex('users').where({ id: req.payload.id, name: req.payload.name }).first();
-        req.login(user, (err) => {
-            if (err) {
-                res.status(500).send('Error logging in');
-                return;
-            }
 
-            res.redirect(basepath.successurl);
-        });
+        loginRedirect(req, res, next, user, { 'successMessage': [ 'OTP setting done successfully' ] });
     } catch (error) {
         res.status(500).send('Error verifying OTP.');
     }
 });
-
-router.get('/discord', requireNonLogin, passport.authenticate('discord'));
-
-router.get('/discord/callback', requireNonLogin, passport.authenticate('discord', {
-    failureRedirect: `${basepath.rootpath}/signin`,
-    successRedirect: basepath.successurl,
-}));
-
-router.get('/google', requireNonLogin, passport.authenticate('google', { scope: ['profile', 'email'] }));
-
-router.get('/google/callback', requireNonLogin, passport.authenticate('google', {
-    failureRedirect: `${basepath.rootpath}/signin`,
-    successRedirect: basepath.successurl,
-}));
-
-router.get('/x', requireNonLogin, passport.authenticate('x'));
-
-router.get('/x/callback', requireNonLogin, passport.authenticate('x', {
-    failureRedirect: `${basepath.rootpath}/signin`,
-    successRedirect: basepath.successurl,
-}));
 
 export default router;
