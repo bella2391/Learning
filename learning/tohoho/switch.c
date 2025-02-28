3c95c02b483a699d349ee57eaaac4c4a08f211df
bella <maekawatakaya10.8@gmail.com>
2025-02-28 20:31:02 +0900

    learning about switch syntax

---
+++ learning/tohoho/switch.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+void main() {
+    int n = 2;
+    switch (n) {
+        case 1:
+            printf("One\n");
+            break;
+        case 2:
+            printf("Two\n");
+            break;
+        default:
+            printf("More\n");
+            break;
+    }
+}
