e0d0ff6ccf4426516452353b084555af1433ee54
bella <maekawatakaya10.8@gmail.com>
2025-03-05 01:35:35 +0900

    learning about output string by using iostream's std

---
+++ learning/c++/intro/hello.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "hello" << ", " << "world!" << endl;
+    // if remove above using declarance, you can write like under one
+    /* std::cout << "hello" << std::endl; */
+}
+
