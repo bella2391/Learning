f0b36eccefa97262235ab204561b13a37c368186
bella <maekawatakaya10.8@gmail.com>
2025-03-05 01:36:05 +0900

    basic class definition

---
+++ learning/c++/intro/class/cat.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Cat {
+    private:
+        string name;
+    public:
+        Cat(string s) {
+            name = s;
+        }
+        void naku() {
+            cout << "にゃあ。俺様は" << name << "だ。" << endl;
+        }
+};
+
+int main() {
+    Cat myCat("Tama");
+    myCat.naku();
+    return 0;
+}
